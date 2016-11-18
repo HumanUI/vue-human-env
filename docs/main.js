455e0ad478bc85bbdd3a5edeb22d9aff17084b99
ChaselWu <minowu@foxmail.com>
2016-11-18 14:04:19 +0800

    New

--- docs/main.js
+++ docs/main.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
 import App from './App'
-import env from '../dist/vue-env'
+import env from '../dist/vue-human-env'
 import envConfig from './env.js'
 
 Vue.use(env, envConfig)
 console.log('When Vue use VueEnv', env.get('APP.TESTING.DEBUG'))
 
+/* eslint-disable no-new */
 new Vue({
-  el: 'body',
+  el: '#app',
+  template: '<App/>',
   components: { App }
 })
